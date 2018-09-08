27ac1a832eade63fac5d8c05f8d5fffb7af1c84f
Mickaël POLLET <mickaelpollet@gmail.com>
2018-09-08 15:42:56 +0200

    - En cours d'implémentation

--- index.php
+++ index.php
@@ -1,27 +1,81 @@
 <?php
+/*************************************
+ * @project: 	Xenomorph
+ * @file:     Index page
+ * @author: 	Mickaël POLLET
+ *************************************/
 
 // Déclaration de l'autoload
 require __DIR__ . '/vendor/autoload.php';
 
-echo "<h1>XENOMORPH - Models : Communication</h1>";
-
-$TestUser = array();
-$TestUser['author'] = 'pollet.m@mipih.fr';
-$TestUser['signatory'] = 'mickaelpollet@gmail.com';
-$TestUser['subject'] = 'test subject';
-$TestUser['body'] = 'test body';
-
-var_dump($TestUser);
-
-$TestMailer = new XMailer();
-
-$TestMailer->setAuthor($TestUser['author']);
-$TestMailer->addSignatory($TestUser['signatory']);
-$TestMailer->setSubject($TestUser['subject']);
-$TestMailer->setBody($TestUser['body']);
 
 
-var_dump($TestMailer);
+try {
+
+	try {
+
+		// Chargement des paramètres vitaux
+		if (!file_exists('system.php')) {
+			$error_message = 'Impossible d\'accéder au script du system du framework.';
+			include ('views/critical_error.vew.php');
+			exit;
+		}
+		require_once('system.php');
+
+		// Chargement des configurations
+		if (!file_exists(SITE_ROOT.XCONFIG_PATH.'XConfig_load.php')) {
+			$error_message = 'Impossible d\'accéder au script de chargement des configurations du framework.';
+			include ('views/critical_error.vew.php');
+			exit;
+		}
+		require_once(SITE_ROOT.XCONFIG_PATH.'XConfig_load.php');
+
+	} catch (PDOException $pdo) {
+		throw new XException('0005'.$pdo->errorInfo[1], 4, array( 0 => $pdo->errorInfo[2] ));
+	}
+} catch (XException $Xe) { // FIN Try
+	// Affichage du message d'erreur
+	switch ($Xe->getCriticity()) {
+		case 1: 	$_SESSION['XMessages']['INFO'][] =		$Xe->getMessage();	break;
+		case 2:		$_SESSION['XMessages']['SUCCESS'][] =	$Xe->getMessage();	break;
+		case 3:		$_SESSION['XMessages']['WARNING'][]	=	$Xe->getMessage();	break;
+		case 4:		$_SESSION['XMessages']['ERROR'][]	=	$Xe->getMessage();	break;
+		case 5:		$_SESSION['XMessages']['ERROR'][] =		$Xe->getMessage();	break;
+		default:	$_SESSION['XMessages']['INFO'][] =		$Xe->getMessage();	break;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "<h1>XENOMORPH - Framework</h1>";
+
+echo "Hello world";
 
 
 ?>
